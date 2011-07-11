1c6d34b5710025a1bb3a96501d4a9010ce946706
Berker Peksag <berker.peksag@gmail.com>
2011-07-11 04:12:41 +0300

    First prototype.

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 # coding: utf-8
 
-import logging
 import os
 import posixpath
 import re
 import sys
+import urllib2
 
-from google.appengine.api import users
-from google.appengine.ext import blobstore
+from django.utils import simplejson as json
 from google.appengine.ext import db
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
@@ -20,31 +18,12 @@ import models
 sys.setrecursionlimit(10000) # SDK fix
 
 
+class Github(object):
+    pass
+
+
 class Handler(webapp.RequestHandler):
-	def __init__(self):
-		self.user = users.get_current_user()
-
-		if self.user:
-			self.url = users.create_logout_url('/')
-			self.is_logged = True
-		else:
-			self.url = users.create_login_url('/')
-			self.is_logged = False
-
-		self.values = {
-			'user': self.user,
-			'url': self.url,
-			'is_logged': self.is_logged
-		}
-
-	def get_user_info(self):
-		if self.is_logged:
-			return models.User.gql('WHERE name=:1', self.user).get()
-		else:
-			return None
-
 	def render(self, file, values = {}):
-		values.update(self.values)
 		path = posixpath.join(posixpath.dirname(__file__), 'templates/%s.html' % file)
 		self.response.out.write(template.render(path, values))
 
@@ -57,21 +36,25 @@ class MainHandler(Handler):
 		self.render('index')
 
 
-class FooHandler(Handler):
-    def get(self):
-        self.write('Lorem ipsum dolor sit amet.')
+class WidgetHandler(Handler):
+    def get(self, username):
+        url = 'https://api.github.com/users/%s' % (username)
+        json_string = urllib2.urlopen(url).read()    
+        self.render('widget', {'user': json.loads(json_string)})
+
+    def post(self):
+        self.write('Save')
 
 
 application = webapp.WSGIApplication(
 	[
         ('/', MainHandler),
-        ('/foo', FooHandler),
+        ('/widget/(\w+)', WidgetHandler),
 	],
-	debug = True
+	debug = os.environ.get('SERVER_SOFTWARE', None).startswith('Devel')
 )
 
 def main():
-	logging.getLogger().setLevel(logging.DEBUG)
 	run_wsgi_app(application)
 
 if __name__ == "__main__":
