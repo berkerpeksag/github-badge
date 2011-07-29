b0bac84411e77dadba31894cfd936ecb62cac1e2
Berker Peksag <berker.peksag@gmail.com>
2011-07-29 14:32:55 +0300

    Fixed code style issues.

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ class GitHub(object):
   
   def __init__(self, user):
     self._username = user
-    self.api_base = self.api_base % self.__dict__
+    self.api_base = self.api_base % (self.__dict__)
   
   def __getattr__(self, name):
     if name not in self._properties:
@@ -35,42 +35,40 @@ class GitHub(object):
     
     if name not in self._cache:
       api_values = self._properties[name]
-      self._cache[name] = json.loads(
-                            urllib2.urlopen(self.api_base % api_values)).read()
-                          )
-    
-    return self._cache[name]
-    
+      self._cache[name] = json.loads(urllib2.urlopen(self.api_base % (api_values))).read()
+
+      return self._cache[name]
+
   def __lang_stat_reducer(stats, lang):
     if lang:
       stats[lang] = stats.setdefault(lang, 0) + 1
+
     return stats
 
   @property
   def language_stats(self):
-    return reduce(
-                   self.__lang_stat_reducer,
-                   (repo["language"] for repo in self.repos), {}
-                  )
+    return reduce(self.__lang_stat_reducer,
+                   (repo['language'] for repo in self.repos), {}
+                 )
 
-  def get_favorite_languages(self, limit=0)
+  def get_favorite_languages(self, limit=0):
     lang_stats = self.language_stats
     fav_langs = sorted(lang_stats, key=lambda l: lang_stats[l], reverse=True)
     return fav_langs[:limit] if limit > 0 else fav_langs
 
 
 class Handler(webapp.RequestHandler):
-	def render(self, file, values = {}):
-		path = posixpath.join(posixpath.dirname(__file__), 'templates/%s.html' % file)
-		self.response.out.write(template.render(path, values))
+  def render(self, file, values={}):
+    path = posixpath.join(posixpath.dirname(__file__), 'templates/%s.html' % file)
+    self.response.out.write(template.render(path, values))
 
-	def write(self, string):
-		self.response.out.write(string)
+  def write(self, string):
+    self.response.out.write(string)
 
 
 class MainHandler(Handler):
-	def get(self):
-		self.render('index')
+  def get(self):
+    self.render('index')
 
 
 class WidgetHandler(Handler):
@@ -82,16 +80,15 @@ class WidgetHandler(Handler):
     self.write('Save')
 
 
-application = webapp.WSGIApplication(
-	[
+application = webapp.WSGIApplication([
     ('/', MainHandler),
     ('/widget/(\w+)', WidgetHandler),
-	],
-	debug = os.environ.get('SERVER_SOFTWARE', None).startswith('Devel')
+  ],
+  debug = os.environ.get('SERVER_SOFTWARE', None).startswith('Devel')
 )
 
 def main():
-	run_wsgi_app(application)
+  run_wsgi_app(application)
 
-if __name__ == "__main__":
-	main()
+if __name__ == '__main__':
+  main()
