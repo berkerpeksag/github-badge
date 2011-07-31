15c950b3c35150e77040d974164939ea2270616d
Burak Yigit Kaya <burak@ninua.com>
2011-07-31 21:52:01 +0300

    Closes #7 Closes #9

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 
-import os
 import logging
+import os
+import operator
 import posixpath
 import re
 import sys
@@ -13,14 +14,28 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-logging.getLogger().setLevel(logging.DEBUG)
+#logging.getLogger().setLevel(logging.DEBUG)
 
 sys.setrecursionlimit(10000) # SDK fix
 
 
 class GitHub(object):
   api_base = 'https://api.github.com/users/%(_username)s%%s'
-  _properties = dict(user=('',), repos=('/repos',))
+  _properties = {
+                  'user': {
+                    'params': ('',),
+                    'default': dict(login='?',
+                                    html_url='#',
+                                    avatar_url='https://a248.e.akamai.net/assets.github.com/images/gravatars/gravatar-140.png',
+                                    name='?',
+                                    blog='#'
+                                   ),
+                  },
+                  'repos': {
+                    'params': ('/repos',),
+                    'default': []
+                  }
+                }
   _cache = None
   __link_parser = re.compile(r'\<([^\>]+)\>;\srel="(\w+)"', re.I or re.U)
 
@@ -31,28 +46,43 @@ class GitHub(object):
   
   @classmethod
   def _fetch_data(cls, url):
-    result = urllib2.urlopen(url)
-    data = json.loads(result.read())
+    try:
+      result = urllib2.urlopen(url)
+    except (urllib2.URLError, urllib2.HTTPError):
+      return None
+
     info = result.info()
+    data = json.loads(result.read())
     if isinstance(data, list) and ("Link" in info):
-      logging.debug("Found links: %s" % info['Link'])
+      #logging.debug("Found links: %s" % info['Link'])
       links = dict(((cls.__link_parser.match(link.strip()).group(2, 1)
                        for link in info['Link'].split(','))))
       if "next" in links:
-        logging.debug("Found next link, fetching: %s" % links['next'])
+        #logging.debug("Found next link, fetching: %s" % links['next'])
         data += cls._fetch_data(links['next'])
     return data
   
   def __getattr__(self, name):
-    logging.debug('Property access for %s' % name)
+    #logging.debug('Property access for %s' % name)
     if name not in self._properties:
       raise AttributeError
 
     if name not in self._cache:
-      api_values = self._properties[name]
-      self._cache[name] = self._fetch_data(self.api_base % api_values)
-      return self._cache[name]
+      prop_info = self._properties[name]
+      api_values = prop_info['params']
+      prop_value = self._fetch_data(self.api_base % api_values)
+      if prop_value:
+        self._cache[name] = prop_value
+      else:
+        return prop_info['default']
+    
+    return self._cache[name] 
 
+  def get_favorite_languages(self, limit=0):
+    lang_stats = self.get_language_stats()
+    fav_langs = sorted(lang_stats, key=lambda l: lang_stats[l], reverse=True)
+    return ', '.join(fav_langs[:limit] if limit > 0 else fav_langs)
+  
   @staticmethod
   def __lang_stat_reducer(stats, lang):
     if lang:
@@ -61,13 +91,11 @@ class GitHub(object):
 
   def get_language_stats(self):
     return reduce(self.__lang_stat_reducer,
-                   (repo['language'] for repo in self.repos), {}
+                  (repo['language'] for repo in self.repos), {}
                  )
-
-  def get_favorite_languages(self, limit=0):
-    lang_stats = self.get_language_stats()
-    fav_langs = sorted(lang_stats, key=lambda l: lang_stats[l], reverse=True)
-    return ', '.join(fav_langs[:limit] if limit > 0 else fav_langs)
+  
+  def get_total_project_watchers(self):
+    return reduce(operator.add, (repo['watchers'] for repo in self.repos), 0)
 
 
 class Handler(webapp.RequestHandler):
@@ -98,10 +126,11 @@ class BadgeHandler(Handler):
 
       output = self.render('badge',
                            {'user': github_data.user,
-                            'languages': github_data.get_favorite_languages(5)
+                            'languages': github_data.get_favorite_languages(5),
+                            'project_followers': github_data.get_total_project_watchers()
                            })
       
-      if not memcache.add(username, output):
+      if 'user' in github_data._cache and not memcache.add(username, output):
         logging.error('Memcache set failed for %s' % username)
 
 
