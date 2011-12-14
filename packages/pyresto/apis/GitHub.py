b16b37ae98cb88b2f89882d9101e8372c2192d41
Burak Yigit Kaya <ben@byk.im>
2011-12-14 10:39:48 +0200

    Updated pyresto to its latest version which supports lazy fetching on "Many" relations.

--- packages/pyresto/apis/GitHub.py
+++ packages/pyresto/apis/GitHub.py
@@ -3,14 +3,16 @@
 import re
 from ..core import *
 
+
 class GitHubModel(Model):
     _host = 'api.github.com'
-
     _link_parser = re.compile(r'\<([^\>]+)\>;\srel="(\w+)"', re.I or re.U)
+
     @classmethod
     def _continuator(cls, response):
         link_val = response.getheader('Link', None)
-        if not link_val: return
+        if not link_val:
+            return
 
         links = dict(((cls._link_parser.match(link.strip()).group(2, 1)
                      for link in link_val.split(','))))
@@ -25,7 +27,8 @@ class Comment(GitHubModel):
 class Commit(GitHubModel):
     _path = '/repos/%(user)s/%(repo)s/commits/%(sha)s'
     _pk = 'sha'
-    comments = Many(Comment, '/repos/%(user)s/%(repo)s/commits/%(commit)s/comments?per_page=100')
+    comments = Many(Comment, '/repos/%(user)s/%(repo)s/commits/%(commit)s'
+                             '/comments?per_page=100')
 
 
 class Branch(GitHubModel):
@@ -38,7 +41,8 @@ Tag = Branch
 class Repo(GitHubModel):
     _path = '/repos/%(user)s/%(name)s'
     _pk = 'name'
-    commits = Many(Commit, '/repos/%(user)s/%(repo)s/commits?per_page=100')
+    commits = Many(Commit, '/repos/%(user)s/%(repo)s/commits?per_page=100',
+                   lazy=True)
     comments = Many(Comment, '/repos/%(user)s/%(repo)s/comments?per_page=100')
     tags = Many(Tag, '/repos/%(user)s/%(repo)s/tags?per_page=100')
     branches = Many(Branch, '/repos/%(user)s/%(repo)s/branches?per_page=100')
@@ -51,5 +55,6 @@ class User(GitHubModel):
 
 
 #Late bindings due to circular references
-Repo.contributors = Many(User, '/repos/%(user)s/%(repo)s/contributors?per_page=100')
+Repo.contributors = Many(User, '/repos/%(user)s/%(repo)s'
+                               '/contributors?per_page=100')
 User.follower_list = Many(User, '/users/%(user)s/followers?per_page=100')
