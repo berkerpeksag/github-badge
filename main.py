880841eed0a86ed5074b4ea3e159d57810680510
unknown <BYK@BYK-Desktop.(none)>
2011-12-20 21:46:55 +0200

    Initial and basic switch to python27 runtime. (progresses #21)

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 import os
 
 from app.core import webapp, MainHandler, BadgeHandler, CacheHandler
-from google.appengine.ext.webapp.util import run_wsgi_app
 
 # Load custom Django template filters
 webapp.template.register_template_library('app.customfilters')
@@ -15,10 +14,3 @@ application = webapp.WSGIApplication([
     ],
     debug=os.environ.get('SERVER_SOFTWARE', None).startswith('Devel')
 )
-
-
-def main():
-    run_wsgi_app(application)
-
-if __name__ == '__main__':
-    main()
