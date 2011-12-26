9ddcac2c3597c5db1543531bc1432541638bc50e
Burak Yigit Kaya <madbyk@gmail.com>
2011-12-26 23:15:59 +0200

    Fixed error in determining if the limit is set or not in the commit checker.

--- app/models.py
+++ app/models.py
@@ -41,7 +41,7 @@ class User(GitHub.User):
 
     @staticmethod
     def __make_commit_recency_checker(recent_than, lim=MAX_COMMITS_PER_BRANCH):
-        counter = count(lim, -1) if not lim else count(1, 0)
+        counter = count(lim, -1) if lim else count(1, 0)
         # if lim is None or 0, then return always 1
         def commit_checker(c):
             return counter.next() > 0 and\
